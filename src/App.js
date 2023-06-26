85c57435e78cf5521f224f3b4e9b6522a9bafe01
MSK4477 <ks7997067@gmail.com>
2023-06-26 13:02:46 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ const App = () => {
       <Card
         cardName="FREE"
         price="$0"
-        description={['Single User', '5GB Storage', "Unlimited Public Projects", "Community Access"]}
+        description={['Single User', '5GB Storage', "Unlimited Public Projects", "Community Access"]}  
         liteColor={["Unlimited Private Projects", "Dedicated Phone Support", "Free Subdomain", "Monthly Status Reports"]}
       />
       <Card
         cardName="PLUS"
         price="$9"
-        description={[<b>5 Users </b>, '50GB Storage', "Unlimited Public Projects", "Community Access", "Dedicated Phone Support", "Free Subdomain"]}
+        description={[<b>5 Users </b>, '50GB Storage', "Unlimited Public Projects", "Community Access", "Unlimited Private Projects", "Dedicated Phone Support", "Free Subdomain"]}
         liteColor={["Monthly Status Reports"]}
       />
       <Card
         cardName="PRO"
         price="$49"
-        description={[<b>Unlimited Users</b>, '150GB Storage', "Unlimited Public Projects", "Community Access", "Unlimited Private Projects", "Dedicated Phone Support", "Unlimited Free Subdomain", "Monthly Status Reports"]}
+        description={[<b>Unlimited Users</b>, '150GB Storage', "Unlimited Public Projects", "Community Access", "Unlimited Private Projects", "Dedicated Phone Support", <span><b>Unlimited</b> Free Subdomain</span>, "Monthly Status Reports"]}
       />
     </div>
   );
